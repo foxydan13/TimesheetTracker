ae5eea3d6481ad5dceb44decc1a72615319b5e1c
Dan Fox HMRC <danfoxhmrc@Dans-MacBook-Air.local>
2017-09-01 15:58:53 +0100

    deom

---
+++ submit.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Ducky and Dan's Tantalising Timesheet Tracker</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>  
+  <h1>new page</h1>
+  <?php
+
+    echo $_POST["comments"];
+  ?> 
+</body>
+</html>
